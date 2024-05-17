fb191b859dc914330602dc314f037a0091f37b05
jrdeval <derval.batistajr@gmail.com>
2024-05-17 13:31:24 -0300

    novo modelo

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -34,7 +34,7 @@ def main(input_filepath, output_filepath):
 
         X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.3, random_state=42)
 
-        model = LinearRegression()
+        model = Ridge()
         model.fit(X_train, y_train)
 
         predictions = model.predict(X_test)
