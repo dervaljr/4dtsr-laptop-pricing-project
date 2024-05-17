11dca438f6f7088dc64a7bb50e02a68097f4fb82
jrdeval <derval.batistajr@gmail.com>
2024-05-17 08:39:12 -0300

    importação inicial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='modelo predição de preço laptops',
+    author='Derval Oliveira',
+    license='',
+)
